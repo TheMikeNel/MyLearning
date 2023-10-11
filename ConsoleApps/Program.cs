74821f14e3872c23bfe45fc87b62a4869e36555b
MikeNelli <themikenel@gmail.com>
2023-10-12 00:17:07 +0500

    Yes

---
+++ ConsoleApps/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            Console.ReadKey();
+            Console.WriteLine("Hello");
+            Console.WriteLine("World");
+            Console.WriteLine("!!!");
+            Console.ReadKey();
+        }
+    }
+}
