e8cc194f0a99a059874ca6f2c4be96f23c1775c5
MikeNelli <themikenel@gmail.com>
2023-10-13 00:08:32 +0500

    Mod2 with comments

--- ConsoleApps/Program.cs
+++ ConsoleApps/Program.cs
@@ -14,10 +14,12 @@ namespace ConsoleApps
             byte physicScores = 5;
             float approxScores = ((float)programScores + (float)mathScores + (float)physicScores) / 3;
 
+            //Задание 1. Вариант №1
             Console.WriteLine($"Full name: {fullName} \nAge: {age} \nEmail: {email} \nProgramming scores: {programScores} \nMath scores: {mathScores} \nPhysicScores {physicScores}");
             Console.WriteLine("Approximate: " + approxScores.ToString("#.#") + "\n");
             Console.ReadKey();
 
+            //Задание 1. Вариант №2
             string fullSet = "Full name: {0} \nAge: {1} \nEmail: {2} \nProg scores: {3} \nMath scores: {4} \nPhysicScores: {5} \nApproximate {6}";
 
             Console.WriteLine(fullSet,
@@ -30,6 +32,7 @@ namespace ConsoleApps
                 approxScores.ToString("#.##"));
             Console.ReadKey();
 
+            //Задание 2
             Console.WriteLine("\nSumm of Scores: {0}\nApproximate Scores: {1}", 
                 (programScores + mathScores + physicScores).ToString(),
                 approxScores.ToString("---> # . ## <---"));
