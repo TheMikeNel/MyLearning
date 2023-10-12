c965f091bccf45893cd9e56c597d2dc4c33b1f08
MikeNelli <themikenel@gmail.com>
2023-10-13 00:03:06 +0500

    Mod2

--- ConsoleApps/Program.cs
+++ ConsoleApps/Program.cs
@@ -6,15 +6,33 @@ namespace ConsoleApps
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string fullName = "Иванов Иван Иванович";
+            byte age = 20;
+            string email = "ivan@mail.ru";
+            byte programScores = 10;
+            byte mathScores = 7;
+            byte physicScores = 5;
+            float approxScores = ((float)programScores + (float)mathScores + (float)physicScores) / 3;
+
+            Console.WriteLine($"Full name: {fullName} \nAge: {age} \nEmail: {email} \nProgramming scores: {programScores} \nMath scores: {mathScores} \nPhysicScores {physicScores}");
+            Console.WriteLine("Approximate: " + approxScores.ToString("#.#") + "\n");
             Console.ReadKey();
-            Console.Write("Hello ");
-            Console.Write("World");
-            Console.Write("!!!");
+
+            string fullSet = "Full name: {0} \nAge: {1} \nEmail: {2} \nProg scores: {3} \nMath scores: {4} \nPhysicScores: {5} \nApproximate {6}";
+
+            Console.WriteLine(fullSet,
+                fullName,
+                age,
+                email,
+                programScores,
+                mathScores,
+                physicScores,
+                approxScores.ToString("#.##"));
             Console.ReadKey();
-            Console.WriteLine("Hello");
-            Console.WriteLine("World");
-            Console.WriteLine("!!!");
+
+            Console.WriteLine("\nSumm of Scores: {0}\nApproximate Scores: {1}", 
+                (programScores + mathScores + physicScores).ToString(),
+                approxScores.ToString("---> # . ## <---"));
             Console.ReadKey();
         }
     }
