5312882f1663de841dedccce3220f0480bc3dd60
MikeNelli <themikenel@gmail.com>
2023-11-06 17:55:01 +0300

    Mod5

---
+++ Mod5_Functions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod5_Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string line = Console.ReadLine();
+            Console.WriteLine("Реверсивный текст: " + ReversWords(line));
+            Console.ReadKey();
+        }
+
+        private static string ReversWords(string text)
+        {
+            string[] words = SeparateLineWords(text); ;
+            string endLine = "";
+
+            for (int i = words.Length - 1; i >= 0; i--) 
+            {
+                endLine += words[i] + " ";
+            }
+
+            return endLine;
+        }
+
+        private static string[] SeparateLineWords(string line)
+        {
+            return line.Split(' ');
+        }
+    }
+}
--- Mod5_Functions/Properties/AssemblyInfo.cs
+++ Mod5_Functions/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("Mod3_3")]
+[assembly: AssemblyTitle("Mod5_Functions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mod3_3")]
+[assembly: AssemblyProduct("Mod5_Functions")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
-[assembly: Guid("d9f59662-de18-4c7c-a308-492b0aae5efd")]
+[assembly: Guid("8ad72f8a-803a-425e-9dc3-c1ac0cbb7d88")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
